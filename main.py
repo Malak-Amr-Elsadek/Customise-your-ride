2b3fc08003ad732002634b1ffc96cea9f0c18736
Malak-Amr-Elsadek <malakelsadek.2015@gmail.com>
2024-11-13 15:25:31 +0300

    Customise your food

---
+++ main.py
@@ -0,0 +1,23 @@
+print("Select option")
+print("1.Pizza")
+print("2.Burger")
+choice=int(input("Enter 1 or 2:"))
+
+if choice==1:
+    print("Select option")
+    print("1.Margrita pizza")
+    print("2.Ranch pizza")
+    pizzachoice=int(input("Enter 1 or 2:"))
+    if pizzachoice==1:
+       print("The margrita pizza is 20$")
+    else:
+       print("The ranch pizza is 20$")
+else:
+    print("Select option")
+    print("1.Cheese burger")
+    print("2.Chicken buger")
+    pizzachoice=int(input("Enter 1 or 2:"))
+    if pizzachoice==1:
+       print("The Cheese burger is 25$")
+    else:
+       print("The Chicken buger is 25$")
